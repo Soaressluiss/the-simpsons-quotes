3829f7f6278592a8b31f38599c7022754efd9db4
Luis Soares <luissoaress.dev@gmail.com>
2023-10-08 16:25:05 -0300

    feat: added routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export const App: React.FC = () => {
-    return <></>;
+    return <> <Outlet/></>;
 };
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Outlet />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+        ],
+    },
+]);
